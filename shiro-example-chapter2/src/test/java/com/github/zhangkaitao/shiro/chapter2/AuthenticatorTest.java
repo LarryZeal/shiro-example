eaed2c894048dbd0af882621f7815334e795cd27
集成-张少昆 <alysom@gmail.com>
2016-06-01 09:06:51 +0800

    common version management

--- shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/AuthenticatorTest.java
+++ shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/AuthenticatorTest.java
@@ -25,80 +25,83 @@ import org.junit.Test;
  */
 public class AuthenticatorTest {
 
-    @Test
-    public void testAllSuccessfulStrategyWithSuccess() {
-        login("classpath:shiro-authenticator-all-success.ini");
-        Subject subject = SecurityUtils.getSubject();
+	@Test
+	public void testAllSuccessfulStrategyWithSuccess(){
+		login("classpath:shiro-authenticator-all-success.ini");
+		Subject subject = SecurityUtils.getSubject();
 
-        //得到一个身份集合，其包含了Realm验证成功的身份信息
-        PrincipalCollection principalCollection = subject.getPrincipals();
-        Assert.assertEquals(2, principalCollection.asList().size());
-    }
+		//得到一个身份集合，其包含了Realm验证成功的身份信息
+		PrincipalCollection principalCollection = subject.getPrincipals();
+		Assert.assertEquals(2, principalCollection.asList().size());
 
-    @Test(expected = UnknownAccountException.class)
-    public void testAllSuccessfulStrategyWithFail() {
-        login("classpath:shiro-authenticator-all-fail.ini");
-    }
+		//看来已支持JDK8
+		principalCollection.forEach(e -> System.out.println(e));
+		principalCollection.forEach(System.out::println);
+	}
 
-    @Test
-    public void testAtLeastOneSuccessfulStrategyWithSuccess() {
-        login("classpath:shiro-authenticator-atLeastOne-success.ini");
-        Subject subject = SecurityUtils.getSubject();
+	@Test( expected = UnknownAccountException.class )
+	public void testAllSuccessfulStrategyWithFail(){
+		login("classpath:shiro-authenticator-all-fail.ini");
+	}
 
-        //得到一个身份集合，其包含了Realm验证成功的身份信息
-        PrincipalCollection principalCollection = subject.getPrincipals();
-        Assert.assertEquals(2, principalCollection.asList().size());
-    }
+	@Test
+	public void testAtLeastOneSuccessfulStrategyWithSuccess(){
+		login("classpath:shiro-authenticator-atLeastOne-success.ini");
+		Subject subject = SecurityUtils.getSubject();
 
-    @Test
-    public void testFirstOneSuccessfulStrategyWithSuccess() {
-        login("classpath:shiro-authenticator-first-success.ini");
-        Subject subject = SecurityUtils.getSubject();
+		//得到一个身份集合，其包含了Realm验证成功的身份信息
+		PrincipalCollection principalCollection = subject.getPrincipals();
+		Assert.assertEquals(2, principalCollection.asList().size());
+	}
 
-        //得到一个身份集合，其包含了第一个Realm验证成功的身份信息
-        PrincipalCollection principalCollection = subject.getPrincipals();
-        Assert.assertEquals(1, principalCollection.asList().size());
-    }
+	@Test
+	public void testFirstOneSuccessfulStrategyWithSuccess(){
+		login("classpath:shiro-authenticator-first-success.ini");
+		Subject subject = SecurityUtils.getSubject();
 
-    @Test
-    public void testAtLeastTwoStrategyWithSuccess() {
-        login("classpath:shiro-authenticator-atLeastTwo-success.ini");
-        Subject subject = SecurityUtils.getSubject();
+		//得到一个身份集合，其包含了第一个Realm验证成功的身份信息
+		PrincipalCollection principalCollection = subject.getPrincipals();
+		Assert.assertEquals(1, principalCollection.asList().size());
+	}
 
-        //得到一个身份集合，因为myRealm1和myRealm4返回的身份一样所以输出时只返回一个
-        PrincipalCollection principalCollection = subject.getPrincipals();
-        Assert.assertEquals(1, principalCollection.asList().size());
-    }
+	@Test
+	public void testAtLeastTwoStrategyWithSuccess(){
+		login("classpath:shiro-authenticator-atLeastTwo-success.ini");
+		Subject subject = SecurityUtils.getSubject();
 
-    @Test
-    public void testOnlyOneStrategyWithSuccess() {
-        login("classpath:shiro-authenticator-onlyone-success.ini");
-        Subject subject = SecurityUtils.getSubject();
+		//得到一个身份集合，因为myRealm1和myRealm4返回的身份一样所以输出时只返回一个
+		PrincipalCollection principalCollection = subject.getPrincipals();
+		Assert.assertEquals(1, principalCollection.asList().size());
+	}
 
-        //得到一个身份集合，因为myRealm1和myRealm4返回的身份一样所以输出时只返回一个
-        PrincipalCollection principalCollection = subject.getPrincipals();
-        Assert.assertEquals(1, principalCollection.asList().size());
-    }
+	@Test
+	public void testOnlyOneStrategyWithSuccess(){
+		login("classpath:shiro-authenticator-onlyone-success.ini");
+		Subject subject = SecurityUtils.getSubject();
 
-    private void login(String configFile) {
-        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-        Factory<org.apache.shiro.mgt.SecurityManager> factory =
-                new IniSecurityManagerFactory(configFile);
+		//得到一个身份集合，因为myRealm1和myRealm4返回的身份一样所以输出时只返回一个
+		PrincipalCollection principalCollection = subject.getPrincipals();
+		Assert.assertEquals(1, principalCollection.asList().size());
+	}
 
-        //2、得到SecurityManager实例 并绑定给SecurityUtils
-        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
-        SecurityUtils.setSecurityManager(securityManager);
+	private void login(String configFile){
+		//1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory(configFile);
 
-        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		//2、得到SecurityManager实例 并绑定给SecurityUtils
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
 
-        subject.login(token);
-    }
+		//3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
 
-    @After
-    public void tearDown() throws Exception {
-        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
-    }
+		subject.login(token);
+	}
+
+	@After
+	public void tearDown() throws Exception{
+		ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+	}
 
 }
